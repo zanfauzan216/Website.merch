01ff9c36fd6e238a292adbb1eb49dc642196e40c
zanfauzan216 <150909540+zanfauzan216@users.noreply.github.com>
2024-07-04 12:30:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const Menuicon=document.getElementById('Menuicon')
+const menu=document.querySelector('.menu')
+const home=document.getElementById('Home')
+const serives=document.getElementById('Services')
+const kontak=document.getElementById('Contact')
+Menuicon.addEventListener('click',function(){
+    menu.classList.toggle('menuop')
+    menu.classList.toggle('display')
+    home.innerText='home';
+})
